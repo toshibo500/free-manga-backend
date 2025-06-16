8d7297815d4796aeea11f8deecb4a4411c96ecfa
toshibo500 <walking.complex@gmail.com>
2025-06-16 17:40:49 +0900

    Add EbookStoreDetailSerializer and integrate ebookstore details into MangaSerializer

--- manga/serializers.py
+++ manga/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Manga, Category
+from .models import Manga, Category, EbookStore, MangaEbookStoreDetailUrl
+from drf_yasg.utils import swagger_serializer_method
 
 class CategorySerializer(serializers.ModelSerializer):
     """カテゴリのシリアライザ"""
@@ -9,16 +10,35 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 
+class EbookStoreDetailSerializer(serializers.Serializer):
+    """電子書籍ストア詳細情報のシリアライザ"""
+    ebookstore_name = serializers.CharField()
+    manga_detail_url = serializers.URLField()
+
+
 class MangaSerializer(serializers.ModelSerializer):
     """マンガのシリアライザ"""
     # カテゴリーをIDのみで表示するため、StringRelatedFieldを使用
     categories = serializers.StringRelatedField(many=True)
+    # 電子書籍ストア情報
+    ebookstores = serializers.SerializerMethodField()
     
     class Meta:
         model = Manga
         fields = [
             'id', 'title', 'isbn', 'author', 'cover_image', 
             'categories', 'description', 'first_book_title', 'rating',
-            'free_chapters', 'free_books'
+            'free_chapters', 'free_books', 'ebookstores'
         ]
         read_only_fields = ['id']
+    
+    @swagger_serializer_method(serializer_or_field=EbookStoreDetailSerializer(many=True))
+    def get_ebookstores(self, obj):
+        detail_urls = MangaEbookStoreDetailUrl.objects.filter(manga=obj).select_related('ebookstore')
+        return [
+            {
+                'ebookstore_name': d.ebookstore.name,
+                'manga_detail_url': d.url
+            }
+            for d in detail_urls
+        ]
